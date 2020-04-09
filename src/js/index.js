ff5352d3c3d5f3d1e2874c094f05170101ba9569
YuriySga <ess1928@yandex.ru>
2020-04-10 00:57:23 +0400

    feat: setup webpack bootstrap 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+//import 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
 import '../css/style.scss';
 import {
--- webpack.config.js
+++ webpack.config.js
@@ -50,12 +50,32 @@ const config = {
           /node_modules/
         ]
       },
-      {
+      /* {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles SASS to CSS
+        }]
+      }, */
+       {
         test: /\.scss$/,
         use: [
             MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' 
         ]
-    },
+    }, 
      /*  {
         test: /\.css$/,
         use: [
