6c1c3d71a5b5d2d44a8e39bab83c924a0cefd3d7
YuriySga <ess1928@yandex.ru>
2020-04-12 23:56:36 +0400

    feat: nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
 import 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
 import '../css/style.scss';
-import {
+/* import {
   moduleOne
 } from './moduleOne';
 
 const helloArr = require('./moduleOne.js');
-
-
 
 class TestClass {
   constructor() {
@@ -19,11 +16,14 @@ class TestClass {
 
 const test = new TestClass();
 
-
 // Пример массива
 console.log(helloArr);
 
-/* пример подключения модуля*/
+/* пример подключения модуля
 let mod = moduleOne(2, 3);
 
 console.log(mod);
+
+ */
+
+ 
