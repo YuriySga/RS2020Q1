0e203f275c81e67e3f1327f7af39df170725a0ea
YuriySga <ess1928@yandex.ru>
2020-05-02 10:40:55 +0400

    feat: update swiper settings

--- movie-search/src/js/index.js
+++ movie-search/src/js/index.js
@@ -22,6 +22,27 @@ const mySwiper = new Swiper ('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
+  /* slidesPerView: 3, */
+  grabCursor: true,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+
  
 });
 
